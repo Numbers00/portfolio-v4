bdc738c3454250c4a0ac578b2d922618f9d6136c
Numbers00 <kmaquino@up.edu.ph>
2022-08-06 21:24:57 +0800

    finished up to about-section

---
+++ script.js
@@ -0,0 +1,13 @@
+const techSlider = tns({
+  container: '.tech-slider',
+  items: 3,
+  slideBy: 'page',
+  autoWidth: true,
+  autoplay: true,
+  autoplayTimeout: 3500,
+  mouseDrag: true,
+  swipeAngle: false,
+  controls: false,
+  nav: false,
+  speed: 400
+});
